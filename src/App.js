1c8a8287490e448b1a8f3d374d4046eaa8031fdf
Sarang <sarangkoroth@gmail.com>
2020-10-01 15:24:29 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="pp">
       <Router>
         <Header />
         <Switch>
-          <Route path="/">
+          <Route path="/home">
             <Body />
             <Foot />
           </Route>
